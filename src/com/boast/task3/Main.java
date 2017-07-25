eaaac116f69aaac52ca2824b0c7eb964ea458888
Oleh Bondarenko <boa.st@ya.ru>
2017-07-25 20:19:31 +0300

    Update Main.java

--- src/com/boast/task3/Main.java
+++ src/com/boast/task3/Main.java
@@ -1,9 +1,8 @@
 package com.boast.task3;
 
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
-import com.boast.Buyer;
+import com.boast.buyer.*;
+import com.boast.menu.ConsoleMenu;
 
 /**
  * Created by Bondarenko Oleh on 24.07.2017.
@@ -13,77 +12,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String menuCommand = "";
+        String menuCommand;
         Scanner in = new Scanner(System.in);
+
         Buyer[] buyers = new Buyer[10];
         for (int i = 0; i < buyers.length; i++) {
             buyers[i] = new Buyer();
         }
 
-        System.out.println("Menu Commands:");
-        System.out.println("show");
-        System.out.println("surnameStartFrom");
-        System.out.println("getCardsFromDiapason");
-        System.out.println("sortByAddress");
-        System.out.println("exit");
+        ConsoleMenu consoleMenu = new ConsoleMenu();
 
-        while (!menuCommand.equals("exit")) {
-            menuCommand = in.nextLine();
-
-            if(menuCommand.equals("show")) {
-                for (Buyer buyer : buyers) {
-                    System.out.println(buyer);
-                }
-            }
-
-            if(menuCommand.equals("surnameStartFrom")) {
-                System.out.print("Start from: ");
-                String start = in.nextLine();
-                boolean nonFlag = true;
-                for (Buyer buyer : buyers) {
-                    if(buyer.getSecondName().startsWith(start)) {
-                        System.out.println(buyer);
-                        nonFlag = false;
-                    }
-                }
-                if(nonFlag) {
-                    System.out.println("No matches");
-                }
-            }
-
-            if(menuCommand.equals("getCardsFromDiapason")) {
-                System.out.print("Diapason start: ");
-                long start = in.nextLong();
-                System.out.print("Diapason end: ");
-                long end = in.nextLong();
-                boolean nonFlag = true;
-                for (Buyer buyer : buyers) {
-                    if(buyer.getCreditCardNumber() >= start && buyer.getCreditCardNumber() <= end) {
-                        System.out.println(buyer);
-                        nonFlag = false;
-                    }
-                }
-                if(nonFlag) {
-                    System.out.println("No matches");
-                }
-            }
-
-            if(menuCommand.equals("sortByAddress")) {
-                Arrays.sort(buyers, new BuyerAddressComparator());
-                for (Buyer buyer : buyers) {
-                    System.out.println(buyer);
-                }
-            }
-        }
-
-    }
-}
-
-class BuyerAddressComparator implements Comparator {
-    @Override
-    public int compare(Object o1, Object o2) {
-        String address1 = ((Buyer) o1).getAddress();
-        String address2 = ((Buyer) o2).getAddress();
-        return address1.compareTo(address2);
+       do {
+           menuCommand = in.nextLine();
+       }  while (consoleMenu.processCommand(buyers, menuCommand));
     }
 }
