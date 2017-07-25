515405838a3e4f85eeabc435d31c4d5b38d45d48
Oleh Bondarenko <boa.st@ya.ru>
2017-07-25 21:36:54 +0300

    Update Buyer.java

--- src/com/boast/buyer/Buyer.java
+++ src/com/boast/buyer/Buyer.java
@@ -16,8 +16,8 @@ public class Buyer {
         secondName = RandomString.getSecondName();
         thirdName = RandomString.getThirdName();
         address = RandomString.getAddress();
-        creditCardNumber = Math.abs(new Random().nextLong()) % 1000;//9999_9999_9999_9999L;
-        bankAccountNumber = Math.abs(new Random().nextLong()) % 999999999999999999L;
+        creditCardNumber = Math.abs(new Random().nextLong()) % 9999_9999_9999_9999L; //1000
+        bankAccountNumber = Math.abs(new Random().nextLong()) % 999_999_999_999_999_999L;
     }
 
     public String getFirstName() {
@@ -46,8 +46,17 @@ public class Buyer {
 
     @Override
     public String toString() {
-        String result = firstName + " " + secondName + " " + thirdName + " " + address;
-        result += "\nCredit Card Number: " + creditCardNumber + " Bank Account Number: " + bankAccountNumber;
+        String zeros = "00000000000000000000";
+
+        String result = firstName + " " + secondName + " " + thirdName;
+        result += "\n  Address: " + address;
+
+        String card = zeros.substring(0, 16 - creditCardNumber.toString().length()) + creditCardNumber;
+        card = card.substring(0, 4) + "_" + card.substring(4, 8) + "_" + card.substring(8, 12) + "_" + card.substring(12, 16);
+
+        String bankNumber = zeros.substring(0, 18 - bankAccountNumber.toString().length()) + bankAccountNumber;
+
+        result += "\n  Credit Card Number: " + card + "\n  Bank Account Number: " + bankNumber;
         return result;
     }
 }
